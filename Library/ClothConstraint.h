f749e3c99dff1f3b6d4ca5efa58d1ea25aced429
Yamada.K <>
2020-02-08 16:18:18 +0900

    add enum stringType in ClothConstraint

--- Library/ClothConstraint.h
+++ Library/ClothConstraint.h
@@ -1,18 +1,25 @@
 #include <iostream>
 #include "Grid.h"
 
+enum stringType{
+    fixed, // fixedはバネの制約を無視して完全な剛体として振る舞う
+    strech,
+    shear,
+    bending,
+    none
+};
+
 struct ClothConstraint {
     Grid grid1;
     Grid grid2;
     float rest; // 質点間のバネの自然長
-    // 0 -> Stretch 1-> Shear2 -> Bending
-    int stringType;  //　TODO: 最終的にはenuにすべきか
+    stringType stringType;
 
     // Initialize
     ClothConstraint() {
         grid1 = Grid();
         grid2 = Grid();
         rest = 0.0;
-        stringtype = 0;
+        stringType = stringType::none;
     }
 };
